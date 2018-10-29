1cc389e8ff27246bcd5e353b65eabb2fd49a48fc
DavidGordon <s01056255@acad.tri-c.edu>
2018-10-29 18:04:31 -0400

    Hello World from David Gordon

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,7 +10,7 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World from David Gordon");
+            Console.Out.WriteLine("Hello World from David Gordon");
         }
     }
 }
