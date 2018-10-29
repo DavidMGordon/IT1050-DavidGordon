609241beeeb70dd313d27ac95b7ce315e4bc9a5b
DavidGordon <s01056255@acad.tri-c.edu>
2018-10-29 17:53:38 -0400

    Fixed the syntax errors, after reading chapter 3.

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-Hello World from David Gordon
+
 namespace Lab01
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World from David Gordon");
         }
     }
 }
